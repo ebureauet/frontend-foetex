d718360178ab826920621be1eef8e4fe487bd69e
teej043 <teej.ebureauet@gmail.com>
2015-04-27 23:08:42 +0800

    updates

--- src/js/main-deps.js
+++ src/js/main-deps.js
@@ -1,2 +1,3 @@
 require('../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+require('../../bower_components/tablesaw/dist/stackonly/tablesaw.stackonly.js');
 require('./general/carousel.js');
