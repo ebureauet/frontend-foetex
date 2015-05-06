7b6536f96147a5b57f64336611047ab91e81c79e
teej043 <teej.ebureauet@gmail.com>
2015-05-06 16:14:34 +0800

    deleted demo files for bootstrap and susy

--- src/js/main-demo-bootstrap.js
+++
@@ -1,4 +0,0 @@
-//These scripts will be bundled
-require('../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
-require('./general/carousel.js');
-//require('./general/custom.js');
--- src/js/main-demo-susy.js
+++
@@ -1,4 +0,0 @@
-//These scripts will be bundled
-
-require('./general/carousel.js');
-require('./general/custom.js');
