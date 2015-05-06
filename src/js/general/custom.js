b86f53e55623493fb5c835da5d134f704bea9227
teej043 <teej.ebureauet@gmail.com>
2015-05-07 03:13:06 +0800

    updates

--- src/js/general/custom.js
+++ src/js/general/custom.js
@@ -3,10 +3,10 @@ $('document').ready(function(){
 });
 
 $(window).load(function() {
-  equalheight('.columnbox--quatre > *');
+  equalheight('.cols--trio > *, .cols--quartet > *');
 });
 
 
 $(window).resize(function(){
-  equalheight('.columnbox--quatre > *');
+  equalheight('.cols--trio > *, .cols--quartet > *');
 });
